9e64ec64aeb1bcca475b583efb02ba60911031d3
Kenneth Kopelson <kennethkopelson@gmail.com>
2021-01-26 13:43:42 -0800

    deploying to git hub pages, and moved static image to src

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Search from "./Search"
+import logo from './images/employeeIcon.PNG'
 //This adds a navbar to the webpage it contains a logo, the title, and a searchbar 
 function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-between">
-        <img src="/images/employeeIcon.PNG" alt="logo"/>
+        <img src={logo} alt="logo"/>
         <a className="navbar-brand" href="/">
         Employee Directory
         </a>
